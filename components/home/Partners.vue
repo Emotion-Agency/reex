da9107d548a3b0d59ba9fe735bee964f15edd735
valeriy-developer <valeriy.gnybidenko@gmail.com>
2025-11-30 20:55:38 +0200

    feat: added marquee to partners section

--- components/home/Partners.vue
+++ components/home/Partners.vue
@@ -1,15 +1,87 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Vue3Marquee } from 'vue3-marquee'
+
+const partners = [
+  {
+    id: 1,
+    img: {
+      src: '/images/partners/roshen.svg',
+      alt: 'Roshen',
+    },
+  },
+  {
+    id: 2,
+    img: {
+      src: '/images/partners/kreisel.svg',
+      alt: 'Kreisel',
+    },
+  },
+  {
+    id: 3,
+    img: {
+      src: '/images/partners/ecosoft.svg',
+      alt: 'Ecosoft',
+    },
+  },
+  {
+    id: 4,
+    img: {
+      src: '/images/partners/epicentr.svg',
+      alt: 'Epicentr',
+    },
+  },
+  {
+    id: 5,
+    img: {
+      src: '/images/partners/univest.svg',
+      alt: 'Univest',
+    },
+  },
+  {
+    id: 6,
+    img: {
+      src: '/images/partners/havi.svg',
+      alt: 'Havi',
+    },
+  },
+  {
+    id: 7,
+    img: {
+      src: '/images/partners/fozzy.svg',
+      alt: 'Fozzy',
+    },
+  },
+  {
+    id: 8,
+    img: {
+      src: '/images/partners/nova-global.svg',
+      alt: 'Nova Global',
+    },
+  },
+]
+</script>
 
 <template>
   <section class="partners">
     <div class="partners__wrapper container">
       <div class="partners__line" />
       <HomePartnersSlider />
+
+      <div class="partners__marquee">
+        <Vue3Marquee clone :duration="8" gradient gradientLength="100px">
+          <img
+            v-for="partner in partners"
+            :key="partner.id"
+            :src="partner.img.src"
+            :alt="partner.img.alt"
+          />
+        </Vue3Marquee>
+      </div>
     </div>
   </section>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss">
 .partners {
   padding-top: vw(144);
 
@@ -22,5 +94,26 @@
   width: 100%;
   height: 1px;
   background-color: var(--foreground-muted-10);
+}
+
+.partners__marquee {
+  margin-top: vw(96);
+
+  @media (max-width: $br1) {
+    margin-top: 72px;
+  }
+
+  .marquee {
+    img {
+      height: auto;
+      max-width: vw(112);
+      margin-right: vw(60);
+
+      @media (max-width: $br1) {
+        max-width: 112px;
+        margin-right: 32px;
+      }
+    }
+  }
 }
 </style>
