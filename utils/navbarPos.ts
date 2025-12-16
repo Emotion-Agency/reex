ea58cc9295650d2220b2d389f3e18c32d4cc5477
valeriy-developer <valeriy.gnybidenko@gmail.com>
2025-12-17 01:18:59 +0200

    feat: updated navbarPos composable

--- utils/navbarPos.ts
+++ utils/navbarPos.ts
@@ -1,73 +1,76 @@
-import { raf } from '@emotionagency/utils'
+import type { IEventArgs } from '@emotionagency/emotion-scroll'
 
-export default class NavbarPos {
-  scrollPos = 0
-  mouseFunc: (e: MouseEvent) => void
-  top = 0
+export const useHeaderInteraction = () => {
+  const isVisible = ref(true)
+  const isHovered = ref(false)
+  const mousePaused = ref(true)
 
-  $sc = document.querySelector('#scroll-container')
-  hovered = false
-
-  constructor() {
-    this.mouseFunc = e => {
-      this.mouseHandler(e)
-    }
-  }
-
-  init() {
-    this.scrollPos = 0
-    this.scrollNav = this.scrollNav.bind(this)
-    raf.on(this.scrollNav)
-  }
-
-  mouseHandler(e) {
-    if (e.screenY <= 300) {
-      document.body.classList.remove('nav-hidden')
-      this.hovered = true
-    } else {
-      this.hovered = false
-      document.body.classList.add('nav-hidden')
-    }
-  }
-
-  scrollNav() {
-    this.top = -this.$sc.scrollTop
-
-    if (this.hovered) {
-      return
-    }
-
-    this.isVisible && this.addVisibility()
-
-    this.isHidden && this.removeVisibility()
-
-    this.scrollPos = -this.$sc.scrollTop
-  }
-
-  get isHidden() {
-    return this.top < this.scrollPos && this.scrollPos < 0 && -this.top >= 0
-  }
-
-  get isVisible() {
-    return this.top > this.scrollPos || this.isFixed
-  }
-
-  get isFixed() {
+  const isFixed = () => {
+    if (getScrollEl().classList.contains('full-page')) return false
     return window.escroll ? window.escroll.disabled : false
   }
 
-  addVisibility() {
-    document.body.classList.remove('nav-hidden')
-    document.removeEventListener('mousemove', this.mouseFunc)
+  const toggleVisible = (value: boolean) => {
+    if (isVisible.value !== value) {
+      isVisible.value = value
+    }
   }
 
-  removeVisibility() {
-    document.body.classList.add('nav-hidden')
-    document.addEventListener('mousemove', this.mouseFunc)
+  const toggleMousePaused = (value: boolean) => {
+    if (mousePaused.value !== value) {
+      mousePaused.value = value
+    }
   }
 
-  destroy() {
-    this.scrollPos = 0
-    raf.off(this.scrollNav)
+  watch(isVisible, value => {
+    document.body.classList.toggle('nav-hidden', !value)
+  })
+
+  const onScroll = (e: IEventArgs) => {
+    if (isHovered.value) return
+
+    const isDisabled =
+      document.documentElement.classList.contains('header-disabled')
+
+    if ((e.direction === 1 && !isFixed() && e.position > 0) || isDisabled) {
+      toggleVisible(false)
+      toggleMousePaused(false)
+    } else {
+      toggleMousePaused(true)
+      toggleVisible(true)
+    }
+  }
+
+  const onMouseMove = (e: MouseEvent) => {
+    if (isFixed() || mousePaused.value || window.innerWidth < 560) return
+
+    if (document.documentElement.classList.contains('header-disabled')) {
+      toggleVisible(false)
+      isHovered.value = false
+      return
+    }
+
+    if (e.screenY <= 200) {
+      toggleVisible(true)
+      isHovered.value = true
+    } else {
+      toggleVisible(false)
+      isHovered.value = false
+    }
+  }
+
+  onMounted(() => {
+    window.escroll.on(onScroll)
+    document.addEventListener('mousemove', onMouseMove)
+  })
+
+  onBeforeUnmount(() => {
+    document.removeEventListener('mousemove', onMouseMove)
+  })
+
+  return {
+    isVisible,
+    isHovered,
+    mousePaused,
   }
 }
