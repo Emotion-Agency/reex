e38cdb1943439f9250ef6abddd465ea51a9e6bae
valeriy-developer <valeriy.gnybidenko@gmail.com>
2026-03-17 20:23:20 +0200

    feat: add gtm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,20 @@ export default defineNuxtConfig({
 
   app: {
     pageTransition: pageTransition as any,
+    head: {
+      script: [
+        {
+          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5CC93VWX');`,
+          tagPosition: 'head',
+        },
+      ],
+      noscript: [
+        {
+          innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5CC93VWX" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          tagPosition: 'bodyOpen',
+        },
+      ],
+    },
   },
 
   typescript: {
